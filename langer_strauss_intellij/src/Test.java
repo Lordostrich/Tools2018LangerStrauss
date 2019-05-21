2a0388c0978c617e8a48ba75eae5d9e4a4fbb59e
Benedikt Langer <S1810237024@students.fh-hagenberg.at>
2019-05-21 12:16:54 +0200

    solvedConflict

--- langer_strauss_intellij/src/Test.java
+++ langer_strauss_intellij/src/Test.java
@@ -3,22 +3,21 @@ import java.util.function.Function;
 
 public class Test {
 
-<<<<<<< HEAD
+
     public void thisIsANewMethod() {
 
-=======
-    public static void main(String[] args){
+
+    }
+        public static void main(String[] args){
 
 
             System.out.println("conflict");
-        System.out.println("This is a Test-Program");
-        System.out.println("Another Line");
+            System.out.println("This is a Test-Program");
+            System.out.println("Another Line");
 
-        System.out.println("Benedikt");
+            System.out.println("Benedikt");
 
+        }
 
-
->>>>>>> 0b5cfa036a02582baf8b94b5fcd8709d3bc73d7e
-    }
 }
 
