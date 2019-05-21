0b5cfa036a02582baf8b94b5fcd8709d3bc73d7e
Benedikt Langer <S1810237024@students.fh-hagenberg.at>
2019-05-21 11:40:10 +0200

    firstChange

--- langer_strauss_intellij/src/Test.java
+++ langer_strauss_intellij/src/Test.java
@@ -1,2 +1,12 @@
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
 public class Test {
+
+    public static void main(String[] args){
+
+
+        System.out.println("This is a Test-Program");
+        System.out.println("Another Line");
+    }
 }
